11e8a491e5f44ede56eb2f251584019c77b77995
Goomiac <giacomo.campa@hotmail.it>
2023-08-26 17:33:01 +0200

    Sketching works properly and can be activated by clicking.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const container = document.querySelector('.container');
 
-function changeColor() {
-    console.log("hover!");
-}
-
+let index = 0;
 for (let i = 0; i < 32; i++) {
     for (let j = 0; j < 32; j++) {
+        index++;
         const row = document.createElement('div');
         row.classList.add('grid-item');
+        row.setAttribute("id",index);
         /*row.innerText = `${i+1},${j+1}`;*/
         container.appendChild(row);
     }
 }
 
 
-//TODO --> the code below doesn't work!
-const gridItem = document.querySelector('.grid-item');
-console.log(gridItem);
-gridItem.addEventListener("hover", changeColor);
-
+const gridItems = document.querySelectorAll('.grid-item');
+let isOn = false;
+gridItems.forEach(item => {
+    item.addEventListener('click', function startDrawing(event) {
+        isOn = !isOn;
+    })
+    item.addEventListener('mouseout', function changeColor(event) {
+        if (isOn) {item.setAttribute('style', 'background-color:black;')};
+    });
+});
