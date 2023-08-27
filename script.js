a1efa0165bcb1eb48c211929f472a8299b42994f
Goomiac <giacomo.campa@hotmail.it>
2023-08-27 17:30:42 +0200

    Add dynamic grid size with fixed width and height.

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.sizeChange');
 
-let index = 0;
-for (let i = 0; i < 32; i++) {
-    for (let j = 0; j < 32; j++) {
-        index++;
-        const row = document.createElement('div');
-        row.classList.add('grid-item');
-        row.setAttribute("id",index);
+button.addEventListener('click', changeGridSize);
+
+function drawGrid(size) {
+    container.style.setProperty('--size', size);
+    for (let i = 0; i < size * size; i++) {
+        let cell = document. createElement('div');;
+        cell.classList.add('grid-item');
         /*row.innerText = `${i+1},${j+1}`;*/
-        container.appendChild(row);
+        container.appendChild(cell).className = "grid-item";
     }
 }
 
+let size = 84;
+drawGrid(size);
+
+function changeGridSize() {
+    let newSize = prompt("Input new grid size: ");
+    container.innerHTML = "";
+    drawGrid(newSize);
+}
 
 const gridItems = document.querySelectorAll('.grid-item');
 let isOn = false;
